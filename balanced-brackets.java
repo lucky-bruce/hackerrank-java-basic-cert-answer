0a1aa88c5cee4d5df088a46523c147541c6ca7a4
lucky-bruce <90888370+lucky-bruce@users.noreply.github.com>
2023-07-13 11:54:56 +0800

    Create balanced-brackets.java

---
+++ balanced-brackets.java
@@ -0,0 +1,52 @@
+static String isBalanced(String s) {
+        Stack<Character> str = new Stack();
+        String ans = "YES";
+        String no = "NO";
+        boolean answer = true;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '{' || ch == '[' || ch == '('){
+                str.push(ch);
+                continue;
+            }
+            if(str.isEmpty()){
+                return no;
+
+            }
+            if(ch == ')'){
+                if(str.peek() == '('){
+                    str.pop();
+                }else{
+                    answer = false;
+                    break;
+                }
+            }
+            if(ch == ']'){
+                if(str.peek() == '['){
+                    str.pop();
+                }else{
+                    answer = false;
+                    break;
+                }
+            }
+            if(ch == '}'){
+                if(str.peek() == '{'){
+                    str.pop();
+                }else{
+                    answer = false;
+                    break;
+                }
+            }
+            
+
+        }
+        if(!str.isEmpty()){
+                answer = false;
+            }
+            if(answer)
+            return ans;
+            else
+            return no;
+
+
+    }
